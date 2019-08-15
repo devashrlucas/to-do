8c0d92e07cd1fdc350f50311608481a8169cdf93
devashrlucas <devashrlucas@gmail.com>
2019-08-15 16:33:26 -0400

    Updated add task function

--- js/index.js
+++ js/index.js
@@ -6,6 +6,11 @@ let taskList = document.getElementById('task-list__tasks');
 
 function addTask() {
     let li = document.createElement('li');
+    let liText = document.createTextNode('');
+    let taskText = document.getElementById('task-list__add').value;
+
+    liText = taskText;
+    li.append(liText);
     document.getElementById('task-list__tasks').append(li);
   
 }
