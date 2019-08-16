acd0df34bbff2a336d482c05ed435c61d5fe7dc3
devashrlucas <devashrlucas@gmail.com>
2019-08-16 10:14:03 -0400

    Started edit task function

--- js/index.js
+++ js/index.js
@@ -1,16 +1,20 @@
 /*
-module.exports.testFunction = function (x) {
+module.exports.testFunction = function(x) {
     return x;
 */
-let taskList = document.getElementById('task-list__tasks');
+
 
 function addTask() {
-    let li = document.createElement('li');
-    let liText = document.createTextNode('');
-    let taskText = document.getElementById('task-list__add').value;
+  let li = document.createElement('li');
+  let liText = document.createTextNode('');
+  let taskText = document.getElementById('task-list__add').value;
 
-    liText = taskText;
-    li.append(liText);
-    document.getElementById('task-list__tasks').append(li);
-  
+  liText = taskText;
+  li.append(liText);
+  document.getElementById('task-list__tasks').append(li);
+  document.getElementById('task-list__tasks').setAttribute('contenteditable', 'false');
+}
+
+function editTask() {
+    document.getElementById('task-list__tasks').setAttribute('contenteditable', 'true');
 }
